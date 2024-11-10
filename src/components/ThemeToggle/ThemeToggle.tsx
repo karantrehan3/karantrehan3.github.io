adba9576d79c6569d8bad2773cf618ac890fcf26
Karan Trehan <41701337+karantrehan3@users.noreply.github.com>
2024-11-10 21:29:40 +0530

    Portfolio v1 (#1)
    * Base Template
    
    * First Version
    
    * Config for deploying react app

---
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import {
+  useMantineColorScheme,
+  useMantineTheme,
+  ActionIcon,
+} from "@mantine/core";
+import { IconSun, IconMoon } from "@tabler/icons-react";
+
+export function ThemeToggle() {
+  const theme = useMantineTheme();
+  const { colorScheme, toggleColorScheme, setColorScheme } =
+    useMantineColorScheme();
+
+  useEffect(() => {
+    const savedColorScheme = localStorage.getItem("mantine-color-scheme-value");
+    if (savedColorScheme) {
+      setColorScheme(savedColorScheme);
+    } else {
+      const prefersDarkScheme = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      setColorScheme(prefersDarkScheme ? "dark" : "light");
+    }
+  }, [setColorScheme]);
+
+  return (
+    <ActionIcon
+      variant="outline"
+      color={
+        colorScheme === "dark" ? theme.colors.yellow[5] : theme.colors.blue[5]
+      }
+      onClick={() => toggleColorScheme()}
+      title="Toggle color scheme"
+      size="lg"
+    >
+      {colorScheme === "dark" ? <IconSun size={23} /> : <IconMoon size={23} />}
+    </ActionIcon>
+  );
+}
