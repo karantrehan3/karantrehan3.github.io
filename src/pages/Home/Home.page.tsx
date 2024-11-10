e1ac398c624211e055e4cfbb88dc903b1087b4ce
Karan Trehan <karan.trehan@springworks.in>
2024-11-10 23:12:01 +0530

    Added handling for Error page

---
+++ src/pages/Home/Home.page.tsx
@@ -0,0 +1,11 @@
+import { Welcome } from "../../components/Welcome/Welcome";
+import { Header } from "../../components/Header/Header";
+
+export function HomePage() {
+  return (
+    <>
+      <Header />
+      <Welcome />
+    </>
+  );
+}
