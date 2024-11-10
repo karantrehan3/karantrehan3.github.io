1f3a2ac84d9220d86287a9b0da72296805391ac2
Karan Trehan <karan.trehan@springworks.in>
2024-11-11 04:57:49 +0530

    Bug Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import "@mantine/core/styles.css";
 
-import React, { useEffect } from "react";
 import { MantineProvider } from "@mantine/core";
 import { Router } from "./Router";
 import { theme } from "./theme";
-import config from "@/utils/Config";
 
 export default function App() {
-  useEffect(() => {
-    document.title = config.get("TITLE.NAME");
-    const link = document.createElement("link");
-    link.rel = "icon";
-    link.type = "image/jpeg";
-    link.href = config.get("LINK.FAV_ICON_PATH");
-    document.head.appendChild(link);
-  }, []);
-
   return (
     <MantineProvider theme={theme}>
       <Router />
