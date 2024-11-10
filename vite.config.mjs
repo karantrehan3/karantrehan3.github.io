94caec910db99fd389ebcc64b80ed1d709138ebe
Karan Trehan <karan.trehan@springworks.in>
2024-11-11 01:44:43 +0530

    Improving on Build

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), visualizer()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.mjs',
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.mjs",
+  },
+  build: {
+    chunkSizeWarningLimit: 500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0];
+          }
+        },
+      },
+    },
   },
 });
