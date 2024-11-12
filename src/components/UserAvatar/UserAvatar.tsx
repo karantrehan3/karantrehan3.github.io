45d2ff5fe13532c9972ea18d15c360f744e12601
Karan Trehan <karan.trehan@springworks.in>
2024-11-13 03:47:02 +0530

    Adding my Avatar to the Home page

---
+++ src/components/UserAvatar/UserAvatar.tsx
@@ -0,0 +1,21 @@
+import { Avatar, Indicator } from "@mantine/core";
+import classes from "./UserAvatar.module.css";
+import DP from "@/assets/dp/profile.jpg";
+
+export function UserAvatar() {
+  return (
+    <div>
+      <Indicator
+        inline
+        label={<span className={classes.label}>👋</span>}
+        size={100}
+        offset={60}
+        position="bottom-end"
+        withBorder={false}
+        className={classes.transparent}
+      >
+        <Avatar src={DP} size="350" />
+      </Indicator>
+    </div>
+  );
+}
--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -1,15 +1,21 @@
 import { Welcome } from "@/components/Welcome/Welcome";
 import { Header } from "@/components/Header/Header";
 import { InProgress } from "@/components/InProgress/InProgress";
+import { UserAvatar } from "@/components/UserAvatar/UserAvatar";
 import classes from "./Home.module.css";
 
 export function HomePage() {
   return (
     <div className={classes.pageContainer}>
       <Header />
-      <div className={classes.center}>
-        <Welcome />
-        <InProgress />
+      <div className={classes.content}>
+        <div className={classes.left}>
+          <UserAvatar />
+        </div>
+        <div className={classes.right}>
+          <Welcome />
+          <InProgress />
+        </div>
       </div>
     </div>
   );
