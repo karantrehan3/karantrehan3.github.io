0513b9fd7bba698f4b58e4186047523d05e5acab
Karan Trehan <karan.trehan@springworks.in>
2024-11-13 04:26:41 +0530

    Added loading to the image avatar

--- src/components/UserAvatar/UserAvatar.tsx
+++ src/components/UserAvatar/UserAvatar.tsx
@@ -1,8 +1,11 @@
-import { Avatar, Indicator } from "@mantine/core";
+import { useState } from "react";
+import { Avatar, Indicator, Loader } from "@mantine/core";
 import classes from "./UserAvatar.module.css";
 import DP from "@/assets/dp/profile.jpg";
 
 export function UserAvatar() {
+  const [loading, setLoading] = useState(true);
+
   return (
     <div>
       <Indicator
@@ -14,7 +17,12 @@ export function UserAvatar() {
         withBorder={false}
         className={classes.transparent}
       >
-        <Avatar src={DP} className={classes.avatar} />
+        {loading && <Loader className={classes.loader} type="dots" size="xl" />}
+        <Avatar
+          src={DP}
+          className={loading ? classes.hidden : classes.avatar}
+          onLoad={() => setLoading(false)}
+        />
       </Indicator>
     </div>
   );
