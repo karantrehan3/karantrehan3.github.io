d77440f407b2e404658c3e9d11987742e0504e7c
Karan Trehan <karan.trehan@springworks.in>
2024-11-13 03:01:10 +0530

    Adding Loaders in required places

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
+import { Loader } from "@mantine/core";
 import config from "@/utils/Config";
 import classes from "./Calendar.module.css";
-
 export function Calendar() {
+  const [loading, setLoading] = useState(true);
+
   return (
     <div>
-      <iframe
-        title="Connect on Calendar"
-        src={config.get("CONTACT.CALENDAR.LINK")}
-        className={classes.iframe}
-      />
+      {loading ? (
+        <div className={classes.center}>
+          <Loader className={classes.loader} type="dots" size="xl" />
+        </div>
+      ) : (
+        <iframe
+          title="Connect on Calendar"
+          src={config.get("CONTACT.CALENDAR.LINK")}
+          className={classes.iframe}
+          onLoad={() => setLoading(false)}
+        />
+      )}
     </div>
   );
 }
