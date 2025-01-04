f1255b1faf995b48c490635230c7f682d7dc2965
Karan Trehan <karan.trehan@springworks.in>
2025-01-04 23:50:55 +0530

    Making Portfolio Scrollable | v1

--- src/pages/Contact/Contact.page.tsx
+++ src/pages/Contact/Contact.page.tsx
@@ -1,20 +1,17 @@
 import { ReactElement } from "react";
-import { PageLayout } from "@/components/PageLayout/PageLayout";
 import { Calendar } from "@/components/Calendar/Calendar";
 import { Socials } from "@/components/Socials/Socials";
 import classes from "./Contact.module.css";
 
-export function ContactPage(): ReactElement {
+export default function ContactPage(): ReactElement {
   return (
-    <PageLayout>
-      <div className={classes.container}>
-        <div className={classes.left}>
-          <Socials />
-        </div>
-        <div className={classes.right}>
-          <Calendar />
-        </div>
+    <div className={classes.container}>
+      <div className={classes.left}>
+        <Socials />
       </div>
-    </PageLayout>
+      <div className={classes.right}>
+        <Calendar />
+      </div>
+    </div>
   );
 }
---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,30 @@
+import { ReactElement, lazy } from "react";
+import { PageLayout } from "../../components/PageLayout/PageLayout";
+import { Suspense } from "../../components/Suspense/Suspense";
+import classes from "./index.module.css";
+
+const HomePage = lazy(() => import("../Home/Home.page"));
+const ProjectsPage = lazy(() => import("../Projects/Projects.page"));
+const ContactPage = lazy(() => import("../Contact/Contact.page"));
+
+export function MainPage(): ReactElement {
+  return (
+    <PageLayout>
+      <div className={classes.section} id="home">
+        <Suspense>
+          <HomePage />
+        </Suspense>
+      </div>
+      <div className={classes.section} id="projects">
+        <Suspense>
+          <ProjectsPage />
+        </Suspense>
+      </div>
+      <div className={classes.section} id="contact">
+        <Suspense>
+          <ContactPage />
+        </Suspense>
+      </div>
+    </PageLayout>
+  );
+}
