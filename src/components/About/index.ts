3ae745cea61d4c30ef54b858f8e3f9a406e2a9a4
Karan Trehan <karantrehan3@gmail.com>
2025-12-22 01:00:49 +0530

    refactor: Use rem over px in CSS

--- src/components/About/index.ts
+++ src/components/About/index.ts
@@ -1,8 +1,6 @@
 export { AboutHeader } from "./AboutHeader";
 export { TypingEffect } from "./TypingEffect";
 export { ExperienceSection } from "./ExperienceSection";
-export { CompanyContent, RoleItem } from "./ExperienceSection";
-export { ProjectItem } from "./ExperienceSection/ProjectItem";
 export { SkillsSection } from "./SkillsSection";
 export { EducationSection } from "./EducationSection";
 export { CollapsibleSection } from "./CollapsibleSection";
